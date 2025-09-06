86e9e899b61f8b0c6c31482fb26b08ba6e65234a
Mark Razem <mrazem@2-A-6.42heilbronn.de>
2025-09-06 02:21:43 +0200

    ctx_split len for new tokens

--- src/split_field/fuckery/ctx_split.c
+++ src/split_field/fuckery/ctx_split.c
@@ -44,11 +44,68 @@ void print_token(t_token *t)
     printf("val: %s\n", t->value);
     printf("ctx: %s\n", t->context);
 }
+void ctx_push_token(t_token **head, t_token **tail, t_token_type type, char *start, int len)
+{
+	t_token	*new;
+
+	new = malloc(sizeof(t_token));
+	if (!new)
+		return ;
+	new->type = type;
+	new->raw = malloc(sizeof(char) * len + 1);
+	if (!new->raw)
+		return ;
+	ft_memcpy(new->raw, start, len);
+	new->raw[len] = '\0';
+	new->value = NULL;
+	new->context = NULL;
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+		*tail = new;
+	}
+	else
+	{
+		(*tail)->next = new;
+		*tail = new;
+	}
+}
 //////////////////////////////////////////////////////////////////////////////////////
 
 static int ft_is_delim(char c)
 {
 	return (c == ' ' || c == '\t' || c == '\n');
+}
+
+// int	ctx_split_len(char *str, char *context, int i)
+// {
+// 	int start;
+// 	start = i;
+
+// 	while(str[i])
+// 	{
+// 		if ((context[i] == '0' || context[i] == 'e') && (ft_is_delim(str[i])))
+// 				break ;
+// 		i++;
+// 	}
+// 	return i - start;
+// }
+
+int	ctx_split_len(char *str, char *context, int i)
+{
+	int len;
+	
+	len = 0;
+
+	while(str[i])
+	{
+		if ((context[i] == '0' || context[i] == 'e') && (ft_is_delim(str[i])))
+			return (len);
+		i++;
+		len++;
+	}
+	return (len);
 }
 
 void ctx_split(t_token *t)
@@ -57,18 +114,28 @@ void ctx_split(t_token *t)
 	int len;
 
 	i = 0;
-	len = 0;
+	// len = 0;
 
 	while(t->value[i])
 	{
+		// printf("%d\n", len);
+		len = ctx_split_len(t->value, t->context, i);
+		if (len > 0)
+		{
+			push_token();
+			i += len;
+		}
+		else
+			i++;
+		printf("%d\n", len);
 		// while (!ft_is_delim(t->value[len]))
 			// len++;
-		while (!ft_is_delim(t->value[len]) || t->context[len] == '0');
-		if (ft_is_delim(t->value[i]) && t->context[i] != '0')
-			printf("%c", '@');
-		else
-			printf("%c", t->value[i]);
-		i++;
+		// while (!ft_is_delim(t->value[len]) || t->context[len] == '0');
+		// if (ft_is_delim(t->value[i]) && t->context[i] != '0')
+		// 	printf("%c", '@');
+		// else
+		// 	printf("%c", t->value[i]);
+		// i++;
 		// if (ft_is_delim(t->value[i]))
 		// {
 		// 	// char c = len + '0';
@@ -96,7 +163,7 @@ int	main(void)
 	
 	// Case 2: \n in value, not
 	char	*value = "aa\nbbb\n\nbb";
-	char	*context = "ee0eee022e";
+	char	*context = "ee0e1e022e";
     t_token	token;
 
 	init_token(&token, raw, value, context);
