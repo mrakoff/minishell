22f2796cdeed27e21fdc199a1d008bad3d4a5ee7
Mark Razem <mrazem@3-I-1.42heilbronn.de>
2025-10-04 03:32:46 +0200

    NORM: expander, heredoc, lexer, parser, split_field

--- src/split_field/split_and_splice.c
+++ src/split_field/split_and_splice.c
@@ -1,20 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   split_and_splice.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrazem <mrazem@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/03 23:51:57 by mrazem            #+#    #+#             */
+/*   Updated: 2025/10/03 23:57:03 by mrazem           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
 static t_token_type	detect_type(char *value)
 {
 	if (!value || !*value)
-		return WORD;
+		return (WORD);
 	if (!ft_strcmp(value, "|"))
-		return PIPE;
+		return (PIPE);
 	if (!ft_strcmp(value, ">"))
-		return T_OUT;
+		return (T_OUT);
 	if (!ft_strcmp(value, "<"))
-		return T_IN;
+		return (T_IN);
 	if (!ft_strcmp(value, ">>"))
-		return T_APPEND;
+		return (T_APPEND);
 	if (!ft_strcmp(value, "<<"))
-		return T_HEREDOC;
-	return WORD;
+		return (T_HEREDOC);
+	return (WORD);
 }
 
 t_token	*ctx_new_token(t_shell *sh, const t_token *old, int start, int len)
@@ -33,35 +45,36 @@ t_token	*ctx_new_token(t_shell *sh, const t_token *old, int start, int len)
 	new->value[len] = '\0';
 	new->context[len] = '\0';
 	new->type = detect_type(new->value);
-	new->raw = old->raw;	//Sharing pointer, since GC will free?
+	new->raw = old->raw;
 	new->was_expanded = true;
 	new->next = NULL;
-	// printf("[split] '%s'\n", new->value);
 	return (new);
 }
 
-void    splice_list(t_token **splice_node, t_token **new_h, t_token **new_t)
+void	splice_list(t_token **splice_node, t_token **new_h, t_token **new_t)
 {
-        t_token *old;
-        t_token *tail;
+	t_token	*old;
+	t_token	*tail;
 
-        if (!splice_node || !new_h || !*new_h)
-                return ;
-        old = *splice_node;
-        tail = (new_t && *new_t) ? *new_t : *new_h;
-        while (tail->next)
-                tail = tail->next;
-        if (old)
-                tail->next = old->next;
-        else
-                tail->next = NULL;
-        *splice_node = *new_h;
-        if (new_h)
-                *new_h = NULL;
-        if (new_t)
-                *new_t = tail;
-        if (old)
-                old->next = NULL;
+	if (!splice_node || !new_h || !*new_h)
+		return ;
+	old = *splice_node;
+	if (new_t && *new_t)
+		tail = *new_t;
+	else
+		tail = *new_h;
+	while (tail->next)
+		tail = tail->next;
+	if (old)
+		tail->next = old->next;
+	else
+		tail->next = NULL;
+	*splice_node = *new_h;
+	*new_h = NULL;
+	if (new_t)
+		*new_t = tail;
+	if (old)
+		old->next = NULL;
 }
 
 static void	init_splice_vars(t_splice *s, t_token **t)
@@ -73,14 +86,14 @@ static void	init_splice_vars(t_splice *s, t_token **t)
 	s->new_tail = NULL;
 }
 
-int ctx_split_to_list(t_shell *sh, t_token **t)
+int	ctx_split_to_list(t_shell *sh, t_token **t)
 {
-    t_splice s;
-    t_token  *new;
+	t_splice	s;
+	t_token		*new;
 
 	if (!t || !*t || !(*t)->value || !(*t)->context)
-		return 0;
-    init_splice_vars(&s, t);
+		return (0);
+	init_splice_vars(&s, t);
 	while (s.old && s.old->value[s.i])
 	{
 		s.len = ctx_split_len(s.old->value, s.old->context, s.i);
@@ -88,18 +101,16 @@ int ctx_split_to_list(t_shell *sh, t_token **t)
 		{
 			new = ctx_new_token(sh, s.old, s.i, s.len);
 			if (!new)
-				return -1;
-            if (new->value[0] != '\0' || is_quoted_empty(new))
-                token_append(&s.new_head, &s.new_tail, new);
+				return (-1);
+			if (new->value[0] != '\0' || is_quoted_empty(new))
+				token_append(&s.new_head, &s.new_tail, new);
 			s.i += s.len;
 		}
 		else
-        {
 			s.i++;
-    	}
-    }
+	}
 	if (!s.new_head || !s.new_tail)
-		return 0;
-    splice_list(t, &s.new_head, &s.new_tail);
-    return 0;
+		return (0);
+	splice_list(t, &s.new_head, &s.new_tail);
+	return (0);
 }
